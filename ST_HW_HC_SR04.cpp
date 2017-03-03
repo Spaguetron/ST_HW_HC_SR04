cea7bfb526f4c1e4f20a71062f9630b80c24815e
Flavio Monteiro <flaviomonteiro2013@gmail.com>
2017-03-03 17:43:40 -0300

    Rename project from TC_Ultrasonic to ST_HW_HC_SR04
    ST_HW_HC_SR04 means:
     * ST - Spaguetron
     * HW - Hardware-specific library
     * HC_SR04 - For HC-SR04 ultrasonic sensors
    
    The copyright owner was also updated to be me (Flávio Monteiro/feikname), as I am the sole author of the code.

---
+++ ST_HW_HC_SR04.cpp
@@ -0,0 +1,131 @@
+/*
+#
+# ST_HW_HC_SR04.cpp
+#
+# (C)2016-2017 Flávio monteiro
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+*/
+
+#include "ST_HW_HC_SR04.h"
+
+ST_HW_HC_SR04::ST_HW_HC_SR04(byte triggerPin, byte echoPin) {
+    this->triggerPin = triggerPin;
+    this->echoPin    = echoPin;
+    this->soundPace  = 26;
+    this->timeout    = 5000;
+
+    pinMode(triggerPin, OUTPUT);
+    pinMode(echoPin,    INPUT);
+}
+
+ST_HW_HC_SR04::ST_HW_HC_SR04(byte triggerPin, byte echoPin, unsigned long timeout) {
+    this->triggerPin = triggerPin;
+    this->echoPin    = echoPin;
+    this->soundPace  = 26;
+    this->timeout    = timeout;
+
+    pinMode(triggerPin, OUTPUT);
+    pinMode(echoPin,    INPUT);
+}
+
+void ST_HW_HC_SR04::setTimeout(unsigned long timeout) {
+    if(timeout == 0) {
+        this->timeout = 5000;
+    }
+
+    this->timeout = timeout;
+}
+
+unsigned long ST_HW_HC_SR04::getTimeout() {
+    return this->timeout;
+}
+
+void ST_HW_HC_SR04::setSoundPace(float pace) {
+    if(pace == 0.0) {
+        this->soundPace = 26;
+        return;
+    }
+
+    this->soundPace = pace;
+}
+
+float ST_HW_HC_SR04::getSoundPace() {
+    return this->soundPace;
+}
+
+float ST_HW_HC_SR04::calculateSoundPace(float distance) {
+    // Send a 10us pulse to the TRIG pin
+    this->triggerPulse();
+
+    // Calculate the elapsed time for the sound to go and return
+    pinMode(this->echoPin, INPUT);
+    int pulseDuration = (int)pulseIn(this->echoPin, HIGH, this->timeout);
+
+    // If the pulse duration is 0, timeout happened
+    if(pulseDuration == 0) {
+        return 0;
+    }
+
+    // Finally, calculate the sound pace to hit (us/cm)
+    float pace = ((float)pulseDuration) / 2.0 / distance;
+
+    // Return the result
+    return pace;
+}
+
+float ST_HW_HC_SR04::getHitDistance() {
+    int hitTime = this->getHitTime();
+
+    if(hitTime == 0) {
+        return 0;
+    }
+
+    float distance = hitTime / this->soundPace;
+
+    return distance;
+}
+
+int ST_HW_HC_SR04::getHitTime() {
+    // Send a 10us pulse to the TRIG pin
+    this->triggerPulse();
+
+    // Calculate the elapsed time for the sound to go and return
+    pinMode(this->echoPin, INPUT);
+    int pulseDuration = (int)pulseIn(this->echoPin, HIGH, this->timeout);
+
+    // If the pulse duration is 0, timeout happened
+    if(pulseDuration == 0) {
+        return 0;
+    }
+
+    // Divide by two, because we are only counting the time the pulse took to go
+    int timeToHit = pulseDuration / 2;
+
+    // Return the result
+    return timeToHit;
+}
+void ST_HW_HC_SR04::triggerPulse() {
+    // Make sure the pin is in output mode
+    pinMode(this->triggerPin, OUTPUT);
+
+    // Make sure the trigger pin is low
+    digitalWrite(this->triggerPin, LOW);
+    delayMicroseconds(2);
+
+    // Send a 10us pulse (as described in the HC-SR04 datasheet)
+    digitalWrite(this->triggerPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(this->triggerPin, LOW);
+}
